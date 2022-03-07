d0201aadca81b963cff84b420c3265324d399202
adrianposadas <posadas.adrian22@gmail.com>
2022-03-07 01:32:49 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://adrianposadas:Bebelabs_22@mongodbserver.dqyfr.mongodb.net/book_list?retryWrites=true&w=majority"
+};
